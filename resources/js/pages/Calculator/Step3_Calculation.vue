2b6be1b8319f532365752c42ba333f8bdbfc6388
Kinkademio <katyamew@gmail.com>
2022-11-17 22:36:31 +1000

    try to fix 1

--- resources/js/pages/Calculator/Step3_Calculation.vue
+++ resources/js/pages/Calculator/Step3_Calculation.vue
@@ -66,28 +66,24 @@
                             </q-popup-edit>
                         </q-td>
                     </q-tr>
-                </template>
-                <template v-slot:body-cell-delete="props">
-                    <q-td :props="props">
-                        <div>
-                            <q-btn
-                                v-if="settings.find(el => el.id === base_settings.find(setting => setting.Название === props.row[0])?.id)"
-                                flat
-                                round
-                                icon="check_box"
-                                class="text-grey-7"
-                                @click="disableCoof(props.row[0])"
-                            />
-                            <q-btn
-                                v-else
-                                flat
-                                round
-                                icon="check_box_outline_blank"
-                                class="text-grey-7"
-                                @click="enableCoof(props.row[0])"
-                            />
-                        </div>
-                    </q-td>
+                    <div>
+                        <q-btn
+                            v-if="settings.find(el => el.id === base_settings.find(setting => setting.Название === props.row[0])?.id)"
+                            flat
+                            round
+                            icon="check_box"
+                            class="text-grey-7"
+                            @click="disableCoof(props.row[0])"
+                        />
+                        <q-btn
+                            v-else
+                            flat
+                            round
+                            icon="check_box_outline_blank"
+                            class="text-grey-7"
+                            @click="enableCoof(props.row[0])"
+                        />
+                    </div>
                 </template>
             </q-table>
 
