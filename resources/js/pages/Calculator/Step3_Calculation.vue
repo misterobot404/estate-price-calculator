c2991937b3750391a51fb307575c39ec27a4cde0
Kinkademio <katyamew@gmail.com>
2022-11-17 22:22:09 +1000

    3.7

--- resources/js/pages/Calculator/Step3_Calculation.vue
+++ resources/js/pages/Calculator/Step3_Calculation.vue
@@ -62,7 +62,7 @@
                         <q-td v-for="(el, keys) in props.row" :key="keys" :props="props">
                             {{ el }}
                             <q-popup-edit v-model="props.row[keys]" v-slot="scope">
-                                <q-input v-model="scope.value" dense autofocus counter @keyup.enter="this.changeValue(props.row.name, scope.value, keys)"></q-input>
+                                <q-input v-model="scope.value" dense autofocus @keyup.enter="this.changeValue(props.row.name, scope.value, keys)"></q-input>
                             </q-popup-edit>
                         </q-td>
                     </q-tr>
